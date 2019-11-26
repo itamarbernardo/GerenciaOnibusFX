d827856e0efa53678447869df03472dd5bf9f33a
Miyyu <49696525+Miyyu@users.noreply.github.com>
2019-11-26 20:03:56 -0300

    Controller sem tratamento de erros

---
+++ src/br/uag/ufrpe/IU/controladores/TelaGerenciarOnibusController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uag.ufrpe.IU.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author miyyu
+ */
+public class TelaGerenciarOnibusController implements Initializable {
+
+    @FXML
+    private ComboBox<?> Motoristas;
+    @FXML
+    private TextField PlacaOnibus;
+    @FXML
+    private TextField TotalPoltronas;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void CadastrarOnibus(ActionEvent event) {
+
+        
+        PlacaOnibus.getText();        
+        TotalPoltronas.getText();
+
+    }
+    
+}
