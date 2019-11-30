495c3ef86d495f04f162aedd91c9995820c4df9a
Jackson <45649262+jacksonlmp@users.noreply.github.com>
2019-11-30 18:20:27 -0300

    Alteraçoes nos Controllers

--- src/br/uag/ufrpe/IU/controladores/CadastrarPassageiroController.java
+++ src/br/uag/ufrpe/IU/controladores/CadastrarPassageiroController.java
@@ -2,10 +2,12 @@ package br.uag.ufrpe.IU.controladores;
 
 import br.uag.ufrpe.negocio.fachada.FachadaFuncionario;
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -20,8 +22,7 @@ import javafx.scene.text.Text;
  * @author Jackson
  */
 public class CadastrarPassageiroController implements Initializable {
-    
-    private final FachadaFuncionario fachadaFuncionario;
+    private FachadaFuncionario fachadaFuncionario;
     
     @FXML
     private TextField labelPassageiroNome;
@@ -36,19 +37,16 @@ public class CadastrarPassageiroController implements Initializable {
     private DatePicker labelPassageiroDataNascimento;
 
     @FXML
-    private Button buttomCadastrar;
-
-    @FXML
     private TextField labelPassageiroRg;
     
     @FXML
     private Label erroRg;
-
+    
     @FXML
-    private RadioButton labelPassageiroIdJovem;
-
+    private RadioButton labelPassageiroIdJovemS;
+    
     @FXML
-    private ToggleGroup IdJovem;
+    private RadioButton labelPassageiroIdJovemN;
 
     @FXML
     private TextField labelPassageiroCpf;
@@ -57,35 +55,77 @@ public class CadastrarPassageiroController implements Initializable {
     private Label erroCpf;
     
     @FXML
-    public void cadastrarPassageiro(ActionEvent event){
+    private Label erroTelefone;
+    
+    @FXML
+    private Button cadastrarPassageiro;
+    
+    @FXML
+    private ToggleGroup IdJovem;
+
+    public CadastrarPassageiroController() {
+        fachadaFuncionario = FachadaFuncionario.getFachadaFuncionario();
+    }
+    
+    @FXML
+    void cadastrarPassageiro(ActionEvent event){
+        Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+        alertaErro.setTitle("Erro");
+        alertaErro.setHeaderText("Erro ao preencher os dados");
+        
+        Alert alertaConfirmacao = new Alert(Alert.AlertType.CONFIRMATION);
+        alertaConfirmacao.setTitle("CONFIRMAÇÃO");
+        alertaConfirmacao.setHeaderText("Confirmação ao Cadastrar");       
+        
         String nomeCompleto = labelPassageiroNome.getText();
         String cpf = labelPassageiroCpf.getText();
         String rg = labelPassageiroRg.getText();
         String telefone = labelPassageiroTelefone.getText();
-        boolean idJovem;
+        String dataNascimento = labelPassageiroDataNascimento.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        boolean idJovem = true;
+        boolean verificaPassageiro = true;    
     
-    
-        if(labelPassageiroIdJovem.isSelected()){
+        if(labelPassageiroIdJovemS.isSelected()){
             idJovem = true;
         }
         idJovem = false;
         
-        if(cpf.length() < 11 && cpf.isEmpty() ){
-            erroCpf.setText("CPF invalido");
+        if(cpf.length() < 11 || cpf.isEmpty() || !cpf.matches("[0-9]*") ){
+            erroCpf.setText("CPF invalido ou insuficiente");
+            verificaPassageiro = false;
         }
         
-        if(rg.isEmpty()){
+        if(rg.isEmpty() || !rg.matches("[0-9]*")){
             erroRg.setText("RG invalido");
+            verificaPassageiro = false;
+        }
+        
+        if(telefone.isEmpty() || !telefone.matches("[0-9]*") || telefone.length() < 7){
+            erroTelefone.setText("Numero Invalido");
+            verificaPassageiro = false;
+        }
+        
+        if (verificaPassageiro) {
+            try {
+                fachadaFuncionario.adicionarPassageiro(nomeCompleto, cpf, rg, telefone, dataNascimento, idJovem);
+                alertaConfirmacao.setAlertType(Alert.AlertType.CONFIRMATION);
+                alertaConfirmacao.setContentText("Passageiro cadastradado com sucesso!");
+                alertaConfirmacao.show();
+
+            } 
+            catch (Exception ex) {
+                ex.printStackTrace();
+                alertaErro.setContentText(ex.getMessage());
+                alertaErro.show();
+            }
+        } 
+        
+        else {
+            alertaErro.setContentText("Erro ao preencher os dados!");
+            alertaErro.show();
         }
         
     }
-    
-    public CadastrarPassageiroController(FachadaFuncionario fachadaFuncionario) {
-        this.fachadaFuncionario = fachadaFuncionario;
-    }
-    
-    
-    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
