b50c81ac819f3e59b289814cc3f5ddd7c7202bfb
Gabriel Felix <53801722+Bielfla27@users.noreply.github.com>
2019-12-01 16:57:20 -0300

    Update RemoverMotoristaController.java

--- src/br/uag/ufrpe/IU/controladores/RemoverMotoristaController.java
+++ src/br/uag/ufrpe/IU/controladores/RemoverMotoristaController.java
@@ -6,11 +6,15 @@
 
 package br.uag.ufrpe.IU.controladores;
 
+import br.uag.ufrpe.negocio.entidades.Motorista;
+import br.uag.ufrpe.negocio.excecoes.motorista.MotoristaNaoExisteException;
+import br.uag.ufrpe.negocio.fachada.FachadaGerente;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -20,23 +24,68 @@ import javafx.scene.control.TextField;
  * @author GABRIEL
  */
 public class RemoverMotoristaController implements Initializable {
+    private  FachadaGerente fachadaGerente;  
+
     @FXML
     private Label cnhMotorista;
+
     @FXML
     private TextField txtCnhMotorista;
+
+    @FXML
+    private Label erroCnh;
+
     @FXML
     private Button removerMotorista;
 
-    /**
-     * Initializes the controller class.
-     */
+ 
+
+    @FXML
+    private void removerMotorista(ActionEvent event) throws MotoristaNaoExisteException {
+        Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+        alertaErro.setTitle("Erro");
+        alertaErro.setHeaderText("Erro ao preencher os dados");
+        
+        Alert alertaConfirmacao = new Alert(Alert.AlertType.CONFIRMATION);
+        alertaConfirmacao.setTitle("CONFIRMAÇÃO");
+        alertaConfirmacao.setHeaderText("Confirmação ao Remover");
+        
+        String cnh = txtCnhMotorista.getText();
+        boolean verifica = true; 
+        
+         if(!cnh.matches(("[0-9]*"))){
+            erroCnh.setText("Apenas numeros");
+            verifica= false; 
+        } 
+           if(verifica){
+            String codigo;
+            Motorista motorista;
+            codigo = txtCnhMotorista.getText();
+            motorista = fachadaGerente.procurarMotorista(codigo);
+            
+            try {              
+                fachadaGerente.removerMotorista(codigo);
+                alertaConfirmacao.setAlertType(Alert.AlertType.CONFIRMATION);
+                alertaConfirmacao.setContentText("Funcionario removido com sucesso!");
+                alertaConfirmacao.show();  
+            } 
+            catch (Exception ex) {
+                ex.printStackTrace();
+                alertaErro.setContentText(ex.getMessage());
+                alertaErro.show();
+            }
+        } 
+        
+        else {
+            alertaErro.setContentText("Dados invalidos ou Motorista Inexistente!");
+            alertaErro.show();
+        }
+        
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-
-    @FXML
-    private void removerMotorista(ActionEvent event) {
-    }
+   
     
 }
