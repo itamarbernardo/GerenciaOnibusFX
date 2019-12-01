f88f0fffd0db633ad10c2d535bf05f6a566dfdda
Gabriel Felix <53801722+Bielfla27@users.noreply.github.com>
2019-11-30 22:07:37 -0300

    Add files via upload

---
+++ src/br/uag/ufrpe/IU/controladores/RemoverMotoristaController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.uag.ufrpe.IU.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author GABRIEL
+ */
+public class RemoverMotoristaController implements Initializable {
+    @FXML
+    private Label cnhMotorista;
+    @FXML
+    private TextField txtCnhMotorista;
+    @FXML
+    private Button removerMotorista;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void removerMotorista(ActionEvent event) {
+    }
+    
+}
