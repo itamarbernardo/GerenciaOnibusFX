33ac73851061c41db206295bb9e6549c27511591
Gabriel Felix <53801722+Bielfla27@users.noreply.github.com>
2019-12-02 01:52:54 -0300

    Update LoginController.java

--- src/br/uag/ufrpe/IU/controladores/LoginController.java
+++ src/br/uag/ufrpe/IU/controladores/LoginController.java
@@ -6,14 +6,18 @@
 
 package br.uag.ufrpe.IU.controladores;
 
+import br.uag.ufrpe.negocio.entidades.Funcionario;
+import br.uag.ufrpe.negocio.fachada.FachadaGerente;
 import java.awt.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
 /**
@@ -22,6 +26,8 @@ import javafx.scene.control.ToggleGroup;
  * @author GABRIEL
  */
 public class LoginController implements Initializable {
+    private FachadaGerente fachadaGerente;
+    
     @FXML
     private Label senha;
 
@@ -32,10 +38,13 @@ public class LoginController implements Initializable {
     private TextField txtLongin;
 
     @FXML
-    private ToggleGroup grup;
+    private RadioButton labelGerente;
 
     @FXML
     private Button entrar;
+
+    @FXML
+    private ToggleGroup grup;
 
     @FXML
     private Label erroCpf;
@@ -46,16 +55,60 @@ public class LoginController implements Initializable {
     @FXML
     private Label erroSenha;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+     public LoginController(){
+        fachadaGerente = FachadaGerente.getFachadaGerente();
+    }  
 
     @FXML
     private void entrar(ActionEvent event) {
+        Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+        alertaErro.setTitle("Erro");
+        alertaErro.setHeaderText("Erro ao preencher os dados");
+        
+        Alert alertaConfirmacao = new Alert(Alert.AlertType.CONFIRMATION);
+        alertaConfirmacao.setTitle("CONFIRMAÇÃO");
+        alertaConfirmacao.setHeaderText("Autenticado!");
+        
+        String cpf = txtLongin.getText();
+        String senha = txtSenha.getText();
+        boolean gerente = true; 
+        Funcionario funcionario; 
+        Funcionario func;
+        boolean verifica = true; 
+        
+         if(labelGerente.isSelected()){
+            gerente = true;
+        } 
+         if(cpf.length() < 11 || cpf.isEmpty() || !cpf.matches("[0-9]*")){
+            erroCpf.setText("Longin invalido");
+            verifica = false;
+        }
+          if(senha.length() < 3 || senha.isEmpty()){
+            erroSenha.setText("Senha invalida ou muito curta");
+            verifica = false;
+        }
+           try {
+                funcionario = fachadaGerente.procurarFuncionario(cpf);
+                func = fachadaGerente.autenticaFuncionario(senha);
+                if(funcionario != null || func != null){
+                    alertaConfirmacao.setAlertType(Alert.AlertType.CONFIRMATION);
+                    alertaConfirmacao.setContentText("Autenticado");
+                    alertaConfirmacao.show();
+                }else {
+                alertaErro.setContentText("Usuário não encontrada!");
+                alertaErro.show();
+            }
+         } catch (Exception ex) {
+            ex.printStackTrace();
+            alertaErro.setContentText(ex.getMessage());
+            alertaErro.show();
+        }
+          
     }
+    
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }  
     
 }
