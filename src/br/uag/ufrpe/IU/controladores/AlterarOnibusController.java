1fd701334d8f090854290680e9003820f4dd5f04
Miyyu <49696525+Miyyu@users.noreply.github.com>
2019-12-02 19:39:07 -0300

    Add files via upload

--- src/br/uag/ufrpe/IU/controladores/AlterarOnibusController.java
+++ src/br/uag/ufrpe/IU/controladores/AlterarOnibusController.java
@@ -7,9 +7,14 @@ package br.uag.ufrpe.IU.controladores;
 
 import br.uag.ufrpe.negocio.entidades.Motorista;
 import br.uag.ufrpe.negocio.entidades.Onibus;
+import br.uag.ufrpe.negocio.excecoes.motorista.MotoristaNaoExisteException;
+import br.uag.ufrpe.negocio.excecoes.onibus.OnibusNaoExisteException;
 import br.uag.ufrpe.negocio.fachada.FachadaGerente;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import static javafx.application.Platform.exit;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,7 +25,7 @@ import javafx.scene.control.TextField;
 /**
  * FXML Controller class
  *
- * @author EMily SAntos
+ * @author Emily Santos
  */
 public class AlterarOnibusController implements Initializable {
     
@@ -31,17 +36,10 @@ public class AlterarOnibusController implements Initializable {
     @FXML
     private Label erroEntrada;
     @FXML
-    private TextField cpfMotorista;
-    @FXML
     private TextField totalPoltronas;
     @FXML
-    private TextField poltronasConvencionais;
-    @FXML
-    private TextField poltronasObeso;
-    @FXML
-    private TextField poltronasReclinaveis;
-    @FXML
-    private TextField poltronasTotalReclinaveis;
+    private TextField cnhMotorista;
+
 
     /**
      * Initializes the controller class.
@@ -54,66 +52,144 @@ public class AlterarOnibusController implements Initializable {
     public AlterarOnibusController(){
         fachadaGerente = fachadaGerente.getFachadaGerente();
     }
+    
+    boolean verificaDados = true;
+    int poltronaConverter = 0;
 
     @FXML
     private void procurarOnibus(ActionEvent event) {
         
-                        
-        /*Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+         Alert alertaErro = new Alert(Alert.AlertType.ERROR);
         alertaErro.setTitle("Erro");
-        alertaErro.setHeaderText("Passagem não existe");
         
-        Alert alertaConfirmacao = new Alert(Alert.AlertType.CONFIRMATION);
-        alertaConfirmacao.setTitle("Confirmação");
-        alertaConfirmacao.setHeaderText("ALterado com sucesso!");
+        verificaDados = true;
         
-        boolean verificaAlterarOnibus;
         
-        String placa = placaOnibus.getText();
+        String placaO = placaOnibus.getText();
         
-        if(codigo.isEmpty()){
-           System.out.println("oiiii");
-            erroEntrada.setText("Entrada Vazia");
-            verificaAlterarOnibus = false;
-           
+        if(placaOnibus.getText().isEmpty()){
+            erroEntrada.setText("Campo Placa vazia");
+            verificaDados = false;
         }
-       else{
-           System.out.println("lllll");
-           erroEntrada.setText("");
-           verificaAlterarOnibus = true;
-       }
+        else{
+            erroEntrada.setText("");
+            verificaDados = true;
+            
+        }
         
-        if(verificaAlterarOnibus){
+        if(verificaDados){
             try{
-                Onibus verificaOnibus = fachadaGerente.procurarOnibus(placa);
+                Onibus verificaOnibus = fachadaGerente.procurarOnibus(placaO);
                 
                 if(verificaOnibus != null){
-                    Motorista motorista = verificaOnibus.getMotorista();
                     
-                    cpfMotorista.setText(motorista.getCpf());
-                    
-                    placaOnibus.setText(placa);
-                    
-                    totalPoltronas.setText(Integer.parseInt(verificaOnibus.getTotalPoltronas()));
-                    
-                    
+                    String cnh = verificaOnibus.getMotorista().getNumeroCarteiraMotorista();
+                    cnhMotorista.setText(cnh);
+                    int totalInt = verificaOnibus.getTotalPoltronas();
+                    String totalPltronas = Integer.toString(totalInt);
+                    totalPoltronas.setText(totalPltronas);
+                }
+                else{
                     
                 }
             }
             catch(Exception ex){
-                
+                alertaErro.setContentText("Erro!");
+                alertaErro.show(); 
             }
         }
-    }
-
-    @FXML
-    private void alterarOnibus(ActionEvent event) {
-    }
-
-    @FXML
-    private void removerOnibus(ActionEvent event) {
-    }*/
+               
     
 }
+
+    @FXML
+    private void alterarOnibus(ActionEvent event) throws MotoristaNaoExisteException {
+        Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+        alertaErro.setTitle("Erro");
+        
+        Alert alertaConfirmacao = new Alert(Alert.AlertType.CONFIRMATION);
+        alertaConfirmacao.setTitle("Confirmação");
+        alertaConfirmacao.setHeaderText("Onibus Alterado com sucesso!");
+        
+        verificaDados = true;
+        
+        if(placaOnibus.getText().isEmpty() || totalPoltronas.getText().isEmpty() || cnhMotorista.getText().isEmpty()){
+            alertaErro.setContentText("Erro Entradas em Branco");
+            alertaErro.show(); 
+            verificaDados = false;
+            return;
+        }
+        
+        String novaPlaca = placaOnibus.getText();
+        String novoCNH = cnhMotorista.getText();
+        String notoPoltronas = totalPoltronas.getText();
+        
+        if(verificaDados){
+            
+        
+            List<Integer> poltronasObeso = new ArrayList<>();
+            poltronasObeso.add(1);
+            poltronasObeso.add(3);
+
+
+            List<Integer> poltronasReclinavel = new ArrayList<>();
+            poltronasReclinavel.add(9);
+            poltronasReclinavel.add(10);
+            poltronasReclinavel.add(11);
+            poltronasReclinavel.add(12);
+
+            List<Integer> poltronasTotalReclinavel = new ArrayList<>();
+            poltronasTotalReclinavel.add(17);
+            poltronasTotalReclinavel.add(18);
+            poltronasTotalReclinavel.add(19);
+            poltronasReclinavel.add(20);
+            
+            Motorista novoM = fachadaGerente.procurarMotorista(novoCNH);
+            
+        
+            try{
+                int novoP = Integer.parseInt(notoPoltronas);
+                
+                fachadaGerente.alterarOnibus(novoM, novaPlaca, novoP, poltronasObeso, poltronasTotalReclinavel, poltronasReclinavel);
+                
+                alertaConfirmacao.show();
+                exit();
+            }
+            catch(Exception ex){
+                alertaErro.setHeaderText("Erro ao alterar os dados de Onibus");
+                alertaErro.show();
+            }
+        }
+        
+        
+    }
+
+    @FXML
+    private void removerOnibus(ActionEvent event) throws OnibusNaoExisteException {
+        
+        Alert alertaErro = new Alert(Alert.AlertType.ERROR);
+        alertaErro.setTitle("Erro");
+        
+        Alert alertaConfirmacao = new Alert(Alert.AlertType.CONFIRMATION);
+        alertaConfirmacao.setTitle("Confirmação");
+        alertaConfirmacao.setHeaderText("Onibus removido com sucesso!");
+        
+        
+        String novaPlaca = placaOnibus.getText();
+        verificaDados = true;
+        
+        Onibus exOnibus = fachadaGerente.procurarOnibus(novaPlaca);
+        
+        if(exOnibus != null){
+            fachadaGerente.removerOnibus(novaPlaca);
+            alertaConfirmacao.setHeaderText("");
+            alertaConfirmacao.show();
+        }
+        else{
+            alertaErro.setHeaderText("Erro ao remover os dados de Onibus");
+            alertaErro.show();
+        }
+        
+    }
     
 }
